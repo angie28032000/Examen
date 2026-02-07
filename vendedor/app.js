cd792c70c82a61d9a69d12190b1040ffe6ff640d
angie28032000 <angie_mora28@hotmail.com>
2026-02-06 21:57:31 -0500

    ejercicio 8 Completa items A, I y J de calcular sueldo

--- vendedor/app.js
+++ vendedor/app.js
@@ -31,6 +31,8 @@ calcularBono = function (ventasMes) {
 
 calcularSueldo=function(){
    //A. recupera la información de nombre y apellido y guarda en variables
+   let nombre = recuperarTexto("txtNombre");
+   let apellido = recuperarTexto("txtApellido");  
    //B. muestra el valor del sueldo base en el div "base"
    mostrarTextoEnDiv("base",SUELDO_BASE);
    //C. recupera la información de la caja de texto de ventas y guarda en una variable
@@ -46,10 +48,16 @@ calcularSueldo=function(){
     //H. con el valor obtenido, invocar a pintarEstrellas
     pintarEstrellas(numeroEstrellas);
     //I. en el div "mensaje" muestra un mensaje <nombre> <apellido> este mes cobrará USD <sueldo>
+    let mensaje = nombre + " " + apellido + " este mes cobrará USD " + total;
+    mostrarTextoEnDiv("mensaje", mensaje);
     //tomando los valores y apellidos ingresados en las cajas y el sueldo calculado incluido los bonos
+    
     //Ejemplo: Juan Perez este mes cobrará USD 560
     //J. Deshabilitar el boton Calcular y las 3 cajas de texto
-    //habilitar el botón Nuevo
+    deshabilitar("btnCalcular");
+    deshabilitar("txtNombre");
+    deshabilitar("txtApellido");
+    deshabilitar("txtVentas");
     habilitar("btnNuevo");
 }
 
